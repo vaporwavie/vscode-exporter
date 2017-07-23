c87740524d623f09a9b5a335a5f77eaaf029cce2
Luiz Nickel <eu@luiznickel.com>
2017-07-22 21:47:54 -0300

    Adds TODO for install every extension, changes structure

--- vscode-exporter.sh
+++ vscode-exporter.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-cat extensions
-
 echo "Options:"
 echo "1 - Install everything"
 echo "2 - Select an extension to install"
 echo "Choose an option: "
-read -r c
+read -r option
 
-if [[ $c = "1" ]]; then
-    echo "TODO install all extensions" # TODO
+if [[ $option = "1" ]]; then
+    echo "Install everything"
+    # @TODO
+    # Do a count to use in a for
+    # Use as parameters the number of strings in the extensions file
+    # ALL=$(extension1 extension2 extension3 ...)
+    # e.g code --install-extension $ALL
 fi
 
-if [[ $c = "2" ]]; then
+if [[ $option = "2" ]]; then
+    cat extensions
+    sleep 1
     echo "Choose an extension: "
-    HOOK=$(cat extensions | grep --color='auto' $c) 
+    read -r extensionSelect
+    HOOK=$(cat extensions | grep --color='auto' $extensionSelect) 
 
     echo "Install extension $HOOK? (Y/y/N/n)"
-    read -r o
+    read -r extensionConfirm
 
-    if [[ $o = "Y" || "y" ]]; then
+    if [[ $extensionConfirm = "Y" || "y" ]]; then
         code --install-extension $HOOK
         echo "Extension installed!"
+    fi
     if [[ $o = "N" || "n" ]]; then
         exit
     fi
-    else
-        echo "Something went wrong. Try again."
-    fi
 fi
-
