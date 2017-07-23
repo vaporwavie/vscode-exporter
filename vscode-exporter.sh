942c55f32be163f40e897a37b51ab207641222b8
Luiz Nickel <eu@luiznickel.com>
2017-07-22 21:38:56 -0300

    Adds extension single install functionality

--- vscode-exporter.sh
+++ vscode-exporter.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-# @TODO 
-# Receive a list of installed extensions
-# Return it to the user
-# Parse from each string so the user can choose what extension to install
-# Install a extension
-# Return success or error
+cat extensions
+
+echo "Options:"
+echo "1 - Install everything"
+echo "2 - Select an extension to install"
+echo "Choose an option: "
+read -r c
+
+if [[ $c = "1" ]]; then
+    echo "TODO install all extensions" # TODO
+fi
+
+if [[ $c = "2" ]]; then
+    echo "Choose an extension: "
+    HOOK=$(cat extensions | grep --color='auto' $c) 
+
+    echo "Install extension $HOOK? (Y/y/N/n)"
+    read -r o
+
+    if [[ $o = "Y" || "y" ]]; then
+        code --install-extension $HOOK
+        echo "Extension installed!"
+    if [[ $o = "N" || "n" ]]; then
+        exit
+    fi
+    else
+        echo "Something went wrong. Try again."
+    fi
+fi
+
