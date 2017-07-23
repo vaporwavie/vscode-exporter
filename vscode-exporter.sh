f6fcd6e7545c895d3beca224032ecd69af411036
Luiz Nickel <eu@luiznickel.com>
2017-07-22 21:10:35 -0300

    Adds TODO

---
+++ vscode-exporter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# @TODO 
+# Receive a list of installed extensions
+# Return it to the user
+# Parse from each string so the user can choose what extension to install
+# Install a extension
+# Return success or error
